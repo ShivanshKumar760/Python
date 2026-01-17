57ebb879a7b4961f9af28b35b98c3a12ea63cf48
Shivansh Kumar <104713531+ShivanshKumar760@users.noreply.github.com>
2026-01-18 03:39:19 +0530

    Update solution for Arithmetic Operators

--- arithmetic-operators/solution.py
+++ arithmetic-operators/solution.py
@@ -6,16 +6,16 @@ Language: python
 Solution by NeetCode GitHub Pusher
 """
 
-a, b, c = 2, 2, 0.5a, b, c = 2, 2, 0.5
+a, b, c = 2, 2, 0.5
 
-addition=a+b+caddition=a+b+c
-print(addition)print(addition)
+addition=a+b+c
+print(addition)
 
-subtract0=0-additionsubtract0=0-addition
-print(subtract0)print(subtract0)
+subtract0=0-addition
+print(subtract0)
 
-multiply=a*b*cmultiply=a*b*c
-print(multiply)print(multiply)
+multiply=a*b*c
+print(multiply)
 
-division=addition/multiplydivision=addition/multiply
-print(division)print(division)
+division=addition/multiply
+print(division)
