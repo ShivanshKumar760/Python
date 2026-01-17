fd8c4bfab30a17d154c3129bc051dd8c65ef2d9f
Shivansh Kumar <104713531+ShivanshKumar760@users.noreply.github.com>
2026-01-18 03:40:47 +0530

    Update solution for Comments

--- comments/solution.py
+++ comments/solution.py
@@ -6,8 +6,8 @@ Language: python
 Solution by NeetCode GitHub Pusher
 """
 
-print("I belong here.")print("I belong here.")
-# print("Why am I here?")# print("Why am I here?")
-# print("Get me out of here!")# print("Get me out of here!")
-print("I also belong here.")print("I also belong here.")
-print("Me too!")print("Me too!")
+print("I belong here.")
+# print("Why am I here?")
+# print("Get me out of here!")
+print("I also belong here.")
+print("Me too!")
