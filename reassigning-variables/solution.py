58a6bd5f379d2e798c2b12b3fe6528e6e907173b
Shivansh Kumar <104713531+ShivanshKumar760@users.noreply.github.com>
2026-01-18 03:43:04 +0530

    Update solution for Reassigning Variables

--- reassigning-variables/solution.py
+++ reassigning-variables/solution.py
@@ -6,14 +6,14 @@ Language: python
 Solution by NeetCode GitHub Pusher
 """
 
-message = "The first message"message = "The first message"
+message = "The first message"
 
-print(message)print(message)
+print(message)
 
-print(message)print(message)
+message = "The second message"
 
-message = "The third message"message = "The third message"
+print(message)
 
-print(message)print(message)
+message = "The third message"
 
-message = "The second message"message = "The second message"
+print(message)
