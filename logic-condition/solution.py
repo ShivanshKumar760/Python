f733e53dda9a3ac8749ace1a9f652d2c3ce3186a
Shivansh Kumar <104713531+ShivanshKumar760@users.noreply.github.com>
2026-01-19 21:11:46 +0530

    Add solution for Logic Condition

---
+++ logic-condition/solution.py
@@ -0,0 +1,20 @@
+"""
+Problem: Logic Condition
+URL: https://neetcode.io/problems/python-logic-condition/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+def discount_applies(age: int) -> bool:
+    if (age < 18 or age >=65):
+        return True
+    else:
+        return False
+
+# don't modify this code below this line
+print(discount_applies(17))
+print(discount_applies(18))
+print(discount_applies(40))
+print(discount_applies(65))
+print(discount_applies(70))
