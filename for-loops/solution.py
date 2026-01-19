a8e9aebf62f034a647aec8e828dcb9eae53eb761
Shivansh Kumar <104713531+ShivanshKumar760@users.noreply.github.com>
2026-01-19 21:53:44 +0530

    Add solution for For Loops

---
+++ for-loops/solution.py
@@ -0,0 +1,10 @@
+"""
+Problem: For Loops
+URL: https://neetcode.io/problems/python-for-loops/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+for i in range(0,13):
+    print(i)
