3176a51715cc9f5c0ffc855e636d50e5a610e28e
Shivansh Kumar <104713531+ShivanshKumar760@users.noreply.github.com>
2026-01-19 21:55:04 +0530

    Add solution for Control Flow

---
+++ control-flow/solution.py
@@ -0,0 +1,27 @@
+"""
+Problem: Control Flow
+URL: https://neetcode.io/problems/python-control-flow/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+for i in range(1, 8):
+    pass
+
+if True:
+    pass
+
+def unfinsished_function():
+    pass
+
+for i in range(1, 8):
+    break
+    print(i)
+
+for i in range(1, 8):
+    # continue
+    break
+    print(i)
+
+print("nothing else happened")
