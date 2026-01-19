02a891dc451333d907f3f88dbef957a1d6523221
Shivansh Kumar <104713531+ShivanshKumar760@users.noreply.github.com>
2026-01-19 21:11:37 +0530

    Add solution for Else-If Statements

---
+++ else-if-statements/solution.py
@@ -0,0 +1,29 @@
+"""
+Problem: Else-If Statements
+URL: https://neetcode.io/problems/python-elif/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+def check_range(num: int) -> str:
+    if(num<0):
+        return "negative"
+    elif(num==0):
+        return "zero"
+    elif(num > 0 and num < 10):
+        return "positive single digit"
+    elif(num>=10):
+        return "positive multi digit"
+
+
+
+
+
+
+  
+# don't modify code below this line
+print(check_range(-10))
+print(check_range(0))
+print(check_range(9))
+print(check_range(1000))
