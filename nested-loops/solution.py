714d50a93f3a36ff258e26ea4ad515cc3e867d57
Shivansh Kumar <104713531+ShivanshKumar760@users.noreply.github.com>
2026-01-19 21:54:42 +0530

    Add solution for Nested Loops

---
+++ nested-loops/solution.py
@@ -0,0 +1,11 @@
+"""
+Problem: Nested Loops
+URL: https://neetcode.io/problems/python-nested-loops/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+for i in range(3,6):
+    for j in range(3,6):
+        print(i,j)
