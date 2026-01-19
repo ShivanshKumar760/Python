ec3ba65f0f3da51412721936109dadbaff5e47bc
Shivansh Kumar <104713531+ShivanshKumar760@users.noreply.github.com>
2026-01-19 20:13:00 +0530

    Add solution for Shorthand Operators

---
+++ shorthand-operators/solution.py
@@ -0,0 +1,19 @@
+"""
+Problem: Shorthand Operators
+URL: https://neetcode.io/problems/python-shorthand-operators/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+number = 0
+
+#1. Add 5 to number and print the result.
+number += 5
+print(number)
+
+#2.Subtract 2 from number and print the result.
+number -= 2
+print(number)
+
+#3. Add number to itself and print the result.
