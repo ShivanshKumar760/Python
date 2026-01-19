6d21983f415bf531fd7cdc8b0ecd7ba50411a969
Shivansh Kumar <104713531+ShivanshKumar760@users.noreply.github.com>
2026-01-19 20:14:14 +0530

    Add solution for Parameters

---
+++ parameters/solution.py
@@ -0,0 +1,15 @@
+"""
+Problem: Parameters
+URL: https://neetcode.io/problems/python-parameters/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+def farewell(name):
+    print("Goodbye, " + name)
+
+farewell("Bob ")
+farewell("Charlie ")
+# don't modify below this line
+farewell("NeetCode ")
