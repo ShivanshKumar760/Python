5fd4f37ad756c1f3127171a08595cb68218431fa
Shivansh Kumar <104713531+ShivanshKumar760@users.noreply.github.com>
2026-01-20 00:21:20 +0530

    Add solution for String Indexing

---
+++ string-indexing/solution.py
@@ -0,0 +1,26 @@
+"""
+Problem: String Indexing
+URL: https://neetcode.io/problems/python-string-indexing/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+def print_first_char(word: str) -> None:
+    print(word[0])
+
+def print_second_char(word: str) -> None:
+    print(word[1])
+
+def print_last_char(word: str) -> None:
+    print(word[len(word)-1])
+
+
+# do not modify below this line
+print_first_char("hello")
+print_second_char("hello")
+print_last_char("hello")
+
+print_first_char("yay")
+print_second_char("yay")
+print_last_char("yay")
