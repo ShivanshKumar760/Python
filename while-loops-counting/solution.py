b4a71d0f081ba0f1c30c48508b0c7e902c2a93e5
Shivansh Kumar <104713531+ShivanshKumar760@users.noreply.github.com>
2026-01-19 21:53:09 +0530

    Add solution for While Loops Counting

---
+++ while-loops-counting/solution.py
@@ -0,0 +1,12 @@
+"""
+Problem: While Loops Counting
+URL: https://neetcode.io/problems/python-while-loops-counting/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+i=0
+while i>=0 and i<=9:
+    print(i)
+    i+=1
