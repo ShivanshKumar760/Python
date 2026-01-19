6afce81b4e0899b6563a773b7ba8b1a153177053
Shivansh Kumar <104713531+ShivanshKumar760@users.noreply.github.com>
2026-01-19 20:18:01 +0530

    Add solution for Default Arguments

---
+++ default-arguments/solution.py
@@ -0,0 +1,13 @@
+"""
+Problem: Default Arguments
+URL: https://neetcode.io/problems/python-default-arguments/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+def greet(name, punctuation="!") -> None:
+    print("Hello, " + name + punctuation)
+
+greet("World", "!")
+greet("World")
