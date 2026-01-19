bed048d3d5089915d7f5d0b21b9067ee37671b14
Shivansh Kumar <104713531+ShivanshKumar760@users.noreply.github.com>
2026-01-19 21:10:00 +0530

    Add solution for If Statements

---
+++ if-statements/solution.py
@@ -0,0 +1,15 @@
+"""
+Problem: If Statements
+URL: https://neetcode.io/problems/python-if/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+def is_balance_low(balance: int) -> None:
+    if(balance<=100):
+        print("Warning: Low balance.")
+# do not modify below this line
+is_balance_low(99)
+is_balance_low(100)
+is_balance_low(101)
