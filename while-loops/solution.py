544fc97bc435a93e7f067a0f1fee2fd8e07e55dc
Shivansh Kumar <104713531+ShivanshKumar760@users.noreply.github.com>
2026-01-19 21:52:41 +0530

    Add solution for While Loops

---
+++ while-loops/solution.py
@@ -0,0 +1,12 @@
+"""
+Problem: While Loops
+URL: https://neetcode.io/problems/python-while-loops/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+i=0
+while i<12:
+    print("I know how to use while loops")
+    i+=1
