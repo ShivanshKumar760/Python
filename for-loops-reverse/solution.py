5fed3373864af6239b4bbe87666d57bf422022c6
Shivansh Kumar <104713531+ShivanshKumar760@users.noreply.github.com>
2026-01-19 21:54:21 +0530

    Add solution for For Loops Reverse

---
+++ for-loops-reverse/solution.py
@@ -0,0 +1,10 @@
+"""
+Problem: For Loops Reverse
+URL: https://neetcode.io/problems/python-for-loops-reverse/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+for i in range(20,9,-1):
+    print(i)
