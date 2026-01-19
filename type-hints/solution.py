ceb811f77b3977cc07ce964f3f65890b2fe6af40
Shivansh Kumar <104713531+ShivanshKumar760@users.noreply.github.com>
2026-01-19 20:16:47 +0530

    Update solution for Type Hints

--- type-hints/solution.py
+++ type-hints/solution.py
@@ -6,9 +6,11 @@ Language: python
 Solution by NeetCode GitHub Pusher
 """
 
-def greet(name: str) -> str:
-    # print("Hello, " + name)
-
+# def greet(name: str) -> str:
+#     # print("Hello, " + name)
+def greet(name:str)->None:
 value= greet("NeetCode")
-    return "Hello, "+name
+#     return "Hello, "+name
+    print("Hello, "+name)
+    
 print(type(value))
