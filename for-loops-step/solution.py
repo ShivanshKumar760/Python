9f7713ca44761dc5476421c83219af2d441c7541
Shivansh Kumar <104713531+ShivanshKumar760@users.noreply.github.com>
2026-01-19 21:54:07 +0530

    Add solution for For Loops Step

---
+++ for-loops-step/solution.py
@@ -0,0 +1,15 @@
+"""
+Problem: For Loops Step
+URL: https://neetcode.io/problems/python-for-loops-step/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+for i in range(0,110,10):
+    print(i)
+
+# i=0
+# while i<110:
+#     print(i)
+#     i+=10
