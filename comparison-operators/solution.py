5a5e0a8e9039fd5f42ff16062e1da74108519747
Shivansh Kumar <104713531+ShivanshKumar760@users.noreply.github.com>
2026-01-19 20:18:53 +0530

    Add solution for Comparison Operators

---
+++ comparison-operators/solution.py
@@ -0,0 +1,19 @@
+"""
+Problem: Comparison Operators
+URL: https://neetcode.io/problems/python-comparison-operators/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+def check_equal(x, y) -> bool:
+    if(x==y):
+        return True
+    else:
+        return False
+
+def check_not_equal(x, y) -> bool:
+    if(x != y):
+        return True
+    else:
+        return False
