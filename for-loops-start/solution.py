091052173ce1487a63c84c7b1f2b28007dcadcf3
Shivansh Kumar <104713531+ShivanshKumar760@users.noreply.github.com>
2026-01-19 21:53:57 +0530

    Add solution for For Loops Start

---
+++ for-loops-start/solution.py
@@ -0,0 +1,11 @@
+"""
+Problem: For Loops Start
+URL: https://neetcode.io/problems/python-for-loops-start/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+
+for i in range(10,21):
+    print(i)
