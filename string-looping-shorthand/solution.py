8a577cf38f3912aeb521c7082b2a7f0091e46ed8
Shivansh Kumar <104713531+ShivanshKumar760@users.noreply.github.com>
2026-01-20 00:21:46 +0530

    Add solution for String Looping Shorthand

---
+++ string-looping-shorthand/solution.py
@@ -0,0 +1,19 @@
+"""
+Problem: String Looping Shorthand
+URL: https://neetcode.io/problems/python-string-looping-shorthand/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+def print_string_characters(word1: str, word2: str) -> None:
+    for char in word1:
+        print(char)
+    for char in word2:
+        print(char)
+
+
+
+
+# do not modify below this line
+print_string_characters("Hello, World!", "Good Job!")
