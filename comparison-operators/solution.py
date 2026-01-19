0ea8b9f460549c0c4b9ac7009ee21a18913db830
Shivansh Kumar <104713531+ShivanshKumar760@users.noreply.github.com>
2026-01-19 21:09:40 +0530

    Update solution for Comparison Operators

--- comparison-operators/solution.py
+++ comparison-operators/solution.py
@@ -6,10 +6,6 @@ Language: python
 Solution by NeetCode GitHub Pusher
 """
 
-def check_equal(x, y) -> bool:
-    if(x==y):
-        return True
-    else:
         return False
 
 def check_not_equal(x, y) -> bool:
@@ -57,6 +53,3 @@ print("3 is greater than 2:", check_greater_than(3, 3))
 
 print("3 is less than or equal to 3:", check_less_than_or_equal(3, 3))
 print("4 is less than or equal to 3:", check_less_than_or_equal(4, 3))
-
-print("3 is greater than or equal to 3:", check_greater_than_or_equal(3, 3))
-print("2 is greater than or equal to 3:", check_greater_than_or_equal(2, 3))
