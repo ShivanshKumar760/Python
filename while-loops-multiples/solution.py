6482ce49ec8570a8153e1f29f7c84640f7ad44cc
Shivansh Kumar <104713531+ShivanshKumar760@users.noreply.github.com>
2026-01-19 21:53:23 +0530

    Add solution for While Loops Multiples

---
+++ while-loops-multiples/solution.py
@@ -0,0 +1,13 @@
+"""
+Problem: While Loops Multiples
+URL: https://neetcode.io/problems/python-while-loops-multiples/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+n=10
+incrementer=1
+while incrementer<=9:
+    print(n*incrementer)
+    incrementer+=1
