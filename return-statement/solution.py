84bc477a989eaab312db97fee22cef25b610071b
Shivansh Kumar <104713531+ShivanshKumar760@users.noreply.github.com>
2026-01-19 20:15:15 +0530

    Add solution for Return Statement

---
+++ return-statement/solution.py
@@ -0,0 +1,16 @@
+"""
+Problem: Return Statement
+URL: https://neetcode.io/problems/python-return-statement/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+def product(a, b):
+    return a*b
+
+# don't modify below this line
+print(product(2, 4))
+print(product(8, 2))
+print(product(4, 8))
+print(product(8, 8))
