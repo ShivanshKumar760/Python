130f1ce61baa1ee5df8fe7d4a480d39b296df7c9
Shivansh Kumar <104713531+ShivanshKumar760@users.noreply.github.com>
2026-01-20 00:21:58 +0530

    Add solution for String Concatenation

---
+++ string-concatenation/solution.py
@@ -0,0 +1,21 @@
+"""
+Problem: String Concatenation
+URL: https://neetcode.io/problems/python-string-concatenation/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+def concatenate(s1: str, s2: str) -> str:
+    if(len(s1)+len(s2)>10):
+        return "Too long!"
+    else:
+        return s1+s2
+
+
+
+
+# do not modify below this line
+print(concatenate("He", "llo"))
+print(concatenate("Hello ", "world!"))
+print(concatenate("Length", "of10"))
