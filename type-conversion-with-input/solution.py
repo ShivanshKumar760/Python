fea23555a70b53ea4d924a2d1670c0455257708c
Shivansh Kumar <104713531+ShivanshKumar760@users.noreply.github.com>
2026-01-21 00:09:03 +0530

    Add solution for Type Conversion with Input

---
+++ type-conversion-with-input/solution.py
@@ -0,0 +1,22 @@
+"""
+Problem: Type Conversion with Input
+URL: https://neetcode.io/problems/python-type-conversion-input/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+def read_integer() -> int:def read_integer() -> int:
+    container=int(input())    container=int(input())
+    return container    return container
+
+
+def read_float() -> float:def read_float() -> float:
+    container=float(input())    container=float(input())
+    return container    return container
+
+
+# do not modify below this line# do not modify below this line
+print(read_integer())print(read_integer())
+print(read_integer())print(read_integer())
+print(read_integer())print(read_integer())
