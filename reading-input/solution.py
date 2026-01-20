148498bd6a1cbdbc33eb28c6f8c91ae22b400f6b
Shivansh Kumar <104713531+ShivanshKumar760@users.noreply.github.com>
2026-01-21 00:08:54 +0530

    Add solution for Reading Input

---
+++ reading-input/solution.py
@@ -0,0 +1,15 @@
+"""
+Problem: Reading Input
+URL: https://neetcode.io/problems/python-read-input/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+def read_and_print_input() -> None:def read_and_print_input() -> None:
+    container=input("Reading one line of text into a string: ")    container=input("Reading one line of text into a string: ")
+    print(container)    print(container)
+
+read_and_print_input()read_and_print_input()
+read_and_print_input()read_and_print_input()
+
