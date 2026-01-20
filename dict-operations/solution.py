8e2f05b3a60a45f42958083a48db178bd04be09e
Shivansh Kumar <104713531+ShivanshKumar760@users.noreply.github.com>
2026-01-21 00:01:28 +0530

    Add solution for Dict Operations

---
+++ dict-operations/solution.py
@@ -0,0 +1,23 @@
+"""
+Problem: Dict Operations
+URL: https://neetcode.io/problems/python-dict-operations/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+your_dict = { your_dict = { 
+  "a": 10,   "a": 10, 
+  "apple": 12,  "apple": 12,
+  "bat": 7  "bat": 7
+}}
+
+
+def pE(entity):def pE(entity):
+    print(entity)    print(entity)
+
+pE(your_dict)pE(your_dict)
+pE(your_dict["a"])pE(your_dict["a"])
+
+check=""check=""
+for i in your_dict:for i in your_dict:
