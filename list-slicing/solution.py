ae1de2efb70d56ac543aa27280355f25c21b0ec6
Shivansh Kumar <104713531+ShivanshKumar760@users.noreply.github.com>
2026-01-20 23:49:22 +0530

    Add solution for List Slicing

---
+++ list-slicing/solution.py
@@ -0,0 +1,17 @@
+"""
+Problem: List Slicing
+URL: https://neetcode.io/problems/python-list-slicing/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+from typing import Listfrom typing import List
+
+def get_last_three_elements(my_list: List[int]) -> List[int]:def get_last_three_elements(my_list: List[int]) -> List[int]:
+    return  my_list[len(my_list)-3:len(my_list)]    return  my_list[len(my_list)-3:len(my_list)]
+
+# do not modify below this line# do not modify below this line
+print(get_last_three_elements([1, 2, 3]))print(get_last_three_elements([1, 2, 3]))
+print(get_last_three_elements([1, 2, 3, 4, 5]))print(get_last_three_elements([1, 2, 3, 4, 5]))
+print(get_last_three_elements([1, 2, 3, 4, 5, 6, 7, 8, 9]))print(get_last_three_elements([1, 2, 3, 4, 5, 6, 7, 8, 9]))
