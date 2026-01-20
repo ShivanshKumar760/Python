3f4254712128cb1d97d15ab279a610f8f1ae3d4e
Shivansh Kumar <104713531+ShivanshKumar760@users.noreply.github.com>
2026-01-21 00:26:24 +0530

    Update solution for Multiple Except Blocks

--- multiple-except-blocks/solution.py
+++ multiple-except-blocks/solution.py
@@ -6,18 +6,18 @@ Language: python
 Solution by NeetCode GitHub Pusher
 """
 
-def divide_numbers(a: str, b: str) -> None:def divide_numbers(a: str, b: str) -> None:
-    try:    try:
+def divide_numbers(a: str, b: str) -> None:
+    try:
+        print(int(a)/int(b))
+    except ValueError:
+        print("Error: Invalid value!")
+    except ZeroDivisionError:
+        print("Error: Division by zero!")
+    except Exception as error:
+        print("An error occurred:", error)
 
 
 
-        print(int(a)/int(b))        print(int(a)/int(b))
-    except ValueError:    except ValueError:
-# do not modify below this line# do not modify below this line
-divide_numbers("10", "2")divide_numbers("10", "2")
-divide_numbers("12", "0")divide_numbers("12", "0")
-        print("Error: Invalid value!")        print("Error: Invalid value!")
-    except ZeroDivisionError:    except ZeroDivisionError:
-        print("Error: Division by zero!")        print("Error: Division by zero!")
-    except Exception as error:    except Exception as error:
-        print("An error occurred:", error)        print("An error occurred:", error)
+# do not modify below this line
+divide_numbers("10", "2")
+divide_numbers("12", "0")
