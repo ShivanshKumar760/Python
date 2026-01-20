dafd9efb3bf0eba3eac0b836fb46664dd41ee323
Shivansh Kumar <104713531+ShivanshKumar760@users.noreply.github.com>
2026-01-21 00:09:54 +0530

    Add solution for Reading Input Practice

---
+++ reading-input-practice/solution.py
@@ -0,0 +1,20 @@
+"""
+Problem: Reading Input Practice
+URL: https://neetcode.io/problems/python-read-input-practice/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+def add_two_numbers() -> int:def add_two_numbers() -> int:
+    line=input()    line=input()
+    splitLine=line.split(",")    splitLine=line.split(",")
+    return int(splitLine[0])+int(splitLine[1])    return int(splitLine[0])+int(splitLine[1])
+
+
+
+# do not modify below this line# do not modify below this line
+print(add_two_numbers())print(add_two_numbers())
+print(add_two_numbers())print(add_two_numbers())
+print(add_two_numbers())print(add_two_numbers())
+print(add_two_numbers())print(add_two_numbers())
