6c704237f13b26b10012c304c4f5cb4ef1ce6888
Shivansh Kumar <104713531+ShivanshKumar760@users.noreply.github.com>
2026-01-22 01:14:36 +0530

    Update solution for Object Attributes

--- object-attributes/solution.py
+++ object-attributes/solution.py
@@ -6,23 +6,23 @@ Language: python
 Solution by NeetCode GitHub Pusher
 """
 
-    def __init__(self, name: str, species: str, hunger: int, energy: int):
-        self.name = name
-        self.species = species
-        self.hunger = hunger
-        self.energy = energy
-
-whiskers = Pet("Whiskers", "cat", 6, 8)
-
 # TODO: Print Whiskers' initial attributes
-print(f"Intial Attributes: {whiskers.name}({whiskers.species}) - Hunger: {whiskers.hunger}, Energy: {whiskers.energy}")
+print(f"Intial Attributes: {whiskers.name} ({whiskers.species}) - Hunger: {whiskers.hunger}, Energy: {whiskers.energy}")
 
 # TODO: Modify Whiskers' attributes:
 #  - Decrease hunger by 3
 #  - Increase energy by 2
-
 whiskers.hunger-=3
 whiskers.energy+=2
+
 # TODO: Print Whiskers' modified attributes
-print(f"Modified Attributes: {whiskers.name}({whiskers.species}) - Hunger: {whiskers.hunger}, Energy: {whiskers.energy}")
+print(f"Modified Attributes: {whiskers.name} ({whiskers.species}) - Hunger: {whiskers.hunger}, Energy: {whiskers.energy}")
+
+whiskers = Pet("Whiskers", "cat", 6, 8)
+
+        self.hunger = hunger
+        self.energy = energy
+        self.species = species
+        self.name = name
+    def __init__(self, name: str, species: str, hunger: int, energy: int):
 class Pet:
