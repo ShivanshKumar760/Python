628b770e028dc38ed7d0c623afdb7da7a041b656
Shivansh Kumar <104713531+ShivanshKumar760@users.noreply.github.com>
2026-01-23 12:12:44 +0530

    Add solution for Docstrings

---
+++ docstrings/solution.py
@@ -0,0 +1,23 @@
+"""
+Problem: Docstrings
+URL: https://neetcode.io/problems/python-docstrings/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+class Pet:
+    def __init__(self, name: str, animal_type: str):
+        self.name = name
+        self.animal_type = animal_type
+
+    """
+    """
+    A class to represent a pet.
+
+    Attributes:
+        name (str): The pet's name
+        animal_type (str): The pet's type
+        """
+        """
+        Initialize a new Pet instance.
