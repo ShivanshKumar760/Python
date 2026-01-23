9069d7cec581be480ae66252fcf8148cc7b589d3
Shivansh Kumar <104713531+ShivanshKumar760@users.noreply.github.com>
2026-01-23 13:15:56 +0530

    Add solution for Public Attribute and Method

---
+++ public-attribute-and-method/solution.py
@@ -0,0 +1,21 @@
+"""
+Problem: Public Attribute and Method
+URL: https://neetcode.io/problems/python-public-attribute-and-method/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+class StoreItem:
+    def __init__(self),name,price):
+       # pass  # Add: name, price
+
+
+chips = StoreItem("Chips", 1.99) # Don't modify this line
+
+# TODO: Access the attributes of the chips object and display them
+
+
+       self.name=name
+       self.price=price
+print(f"Item: {chips.name} - Price: ${chips.price})
