c69f3a9e3df5658bb14a5be6313d8c164b662302
Shivansh Kumar <104713531+ShivanshKumar760@users.noreply.github.com>
2026-01-23 12:16:32 +0530

    Add solution for Code

---
+++ code/solution.py
@@ -0,0 +1,23 @@
+"""
+Problem: Code
+URL: https://neetcode.io/problems/python-implement-superhero-class/solution
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+class SuperHero:
+    """
+    A class to represent a superhero.
+
+    Attributes:
+        name (str): The superhero's name
+        power (str): The superhero's main superpower
+        health (int): The superhero's health points
+    """
+
+    def __init__(self, name: str, power: str, health: int):
+        # TODO: Initialize the superhero's attributes here
+        self.name=name
+        self.power=power
+        self.health=health
