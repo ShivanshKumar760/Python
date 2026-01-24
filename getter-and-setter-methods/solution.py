27bc2d7cde8e16a013d203ef5e6ade00b1d614c9
Shivansh Kumar <104713531+ShivanshKumar760@users.noreply.github.com>
2026-01-25 00:27:59 +0530

    Update solution for Getter and Setter Methods

--- getter-and-setter-methods/solution.py
+++ getter-and-setter-methods/solution.py
@@ -6,7 +6,6 @@ Language: python
 Solution by NeetCode GitHub Pusher
 """
 
-    def get_balance(self)->int:
     # TODO: Add setter method for balance
 
 
@@ -15,14 +14,14 @@ Solution by NeetCode GitHub Pusher
 # Don't modify the code below this line
 account = BankAccount(1000)
         return self.__balance
-    def set_balance(new_balance:int)->None:
+    def set_balance(self,new_balance:int)->None:
         if(new_balance<0):
             print("Cannot set negative balance!")
         self.__balance=new_balance
 print(account.get_balance())
-    # TODO: Add getter method for balance
-
 account.set_balance(-100)
 print(account.get_balance())
 account.set_balance(100)
+print(account.get_balance())
+account.set_balance(0)
 print(account.get_balance())
