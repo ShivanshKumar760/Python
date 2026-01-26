f767c5447c10c2a1efca85790cbd0283c69e82bb
Shivansh Kumar <104713531+ShivanshKumar760@users.noreply.github.com>
2026-01-26 17:40:01 +0530

    Add solution for Class vs Instance Attributes

---
+++ class-vs-instance-attributes/solution.py
@@ -0,0 +1,30 @@
+"""
+Problem: Class vs Instance Attributes
+URL: https://neetcode.io/problems/python-class-vs-instance-attributes/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+class BankAccount: 
+    # TODO: Add class and instance attributes at their appropriate places
+    total_account=0
+    total_balance=0
+
+    def __init__(self,name,balance) -> None:
+        self.name=name
+        self.balance=balance
+        total_account+=1
+        total_balance+=balance
+
+
+
+# TODO: Create two accounts
+# TODO: Print the information using the mentioned format
+alice =BankAccount("Alice",1000)
+bob = BankAccount("Bob",2000)
+
+print(f"Alice's balance: ${alice.balance}")
+print(f"Bob's balance: ${bob.balance}")
+print(f"Total Accounts: {BankAccount.total_accounts}")
+print(f"Total Balance: ${BankAccount.total_balance}")
