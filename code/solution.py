da925a5c36d0ee2eff556dc18df1bab991db975d
Shivansh Kumar <104713531+ShivanshKumar760@users.noreply.github.com>
2026-01-27 22:58:25 +0530

    Update solution for Code

--- code/solution.py
+++ code/solution.py
@@ -1,57 +1,43 @@
 """
 Problem: Code
-URL: https://neetcode.io/problems/python-hide-details-using-decorators/solution
+URL: https://neetcode.io/problems/python-method-overriding/solution
 Language: python
 
 Solution by NeetCode GitHub Pusher
 """
 
-class SuperHero:
-    def __init__(self, name: str, health: int, power_level: int):
+# class Animal:
+#     def __init__(self, name: str):
+#         self.name = name
+
+#     def make_sound(self) -> None:
+#         print("Animal makes a sound")
+
+# # TODO: Create a Dog class and a Cat class that inherit from the `Animal` class.
+# # TODO: Override the `make_sound` method in the `Dog` class to make the dog bark.
+# # TODO: Override the `make_sound` method in the `Cat` class to make the cat meow.
+class Animal:
+    def __init__(self, name: str):
         self.name = name
-        self.__health = health
-        self.__power_level = power_level
 
-    # TODO: Add the getter and setter methods
-    @property
-    def health(self)->int:
-        return self.__health
-    # Remember to use the @property decorator for the getter methods
-    # Remember to use the @setter decorator for the setter methods
-    @health.setter
-    def health(self,health:int)->None:
-        if health>100:
-            print("You can't set the health to more than 100")
-        elif health<0:
-            print("You can't set the health to less than 0")
-        else:
-            self.__health=health
-    @property
-    def power_level(self)->int:
-        return self.__power_level;
+    def make_sound(self) -> None:
+        print("Animal makes a sound")
 
-    @power_level.setter
-    def power_level(self, power_level: int) -> None:
-        if power_level > 10:
-            print("You can't set the power level to more than 10")
-        elif power_level < 1:
-            print("You can't set the power level to less than 1")
-        else:
-            self.__power_level = power_level
+class Dog(Animal):
+    def make_sound(self) -> None:
+        print(f"{self.name} is barking")
+
+class Cat(Animal):
+    def make_sound(self) -> None:
+        print(f"{self.name} is meowing")
+=
 
 
 
-
-# Don't change the following code
-super_hero = SuperHero("Batman", 80, 9)
-
-print(super_hero.health) # this should print 80
-super_hero.health = 110 # this should print You can't set the health to more than 100
-
-print(super_hero.power_level) # this should print 9
-super_hero.power_level = 100 # this should print You can't set the power level to more than 10
-super_hero.power_level = 0 # this should print You can't set the power level to less than 1
-
-
-# TODO: print the hero's attributes 
-print(f"{super_hero.name} has {super_hero.health} health and {super_hero.power_level} power level")
+# Do not modify the code below
+animal = Animal("Cow")
+animal.make_sound() # Output: Animal makes a sound
+dog = Dog("Max")
+dog.make_sound() # Output: Max is barking
+cat = Cat("Luna")
+cat.make_sound() # Output: Luna is meowing
