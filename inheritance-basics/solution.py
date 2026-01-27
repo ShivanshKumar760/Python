67bb9b4d7e5b38354d026679064aa9447416a3a6
Shivansh Kumar <104713531+ShivanshKumar760@users.noreply.github.com>
2026-01-27 22:56:23 +0530

    Update solution for Inheritance Basics

--- inheritance-basics/solution.py
+++ inheritance-basics/solution.py
@@ -12,12 +12,12 @@ class SmartDevice:
 
 # TODO: Implement the SmartLight class
 class SmartLight(SmartDevice):
+    def turn_on(self):
+        print(f"{self.name} is turned on")
+    def turn_off(self):
+        print(f"{self.name} is turned off")
 
 # Don't change the code below
 device = SmartLight("Smart Light")
 device.turn_on()
 device.turn_off()
-    def turn_on(self):
-        print(f"{self.name} is turned on")
-    def turn_off(self):
-        print(f"{self.name} is turned off")
