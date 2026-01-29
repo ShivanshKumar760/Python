642307ca2a8111528f4cdd78b8d6e949365e5afd
Shivansh Kumar <104713531+ShivanshKumar760@users.noreply.github.com>
2026-01-29 13:01:35 +0530

    Add solution for Duck Typing

---
+++ duck-typing/solution.py
@@ -0,0 +1,35 @@
+"""
+Problem: Duck Typing
+URL: https://neetcode.io/problems/python-duck-typing/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+class SpiderMan:
+    def attack(self) -> str:
+        return "Web Shooter!"
+
+    def defend(self) -> str:
+        return "Spider Sense!"
+
+# TODO: Create the BlackWidow class with attack() and defend() methods
+def battle_sequence(hero) -> None:
+class BlackWidow:
+    def attack(self) -> str:
+        return "Widow's Bite!"
+
+    def defend(self) -> str:
+        return "Acrobatic Dodge!"
+
+# TODO: Create the battle_sequence() function
+
+
+# Don't modify the code below
+spider_man = SpiderMan()
+black_widow = BlackWidow()
+
+battle_sequence(spider_man)
+    print(hero.attack())
+    print(hero.defend()
+battle_sequence(black_widow)
