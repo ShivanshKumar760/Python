f32d175908c6e0b088be6d9cc8d22d4533624e96
Shivansh Kumar <104713531+ShivanshKumar760@users.noreply.github.com>
2026-01-29 11:27:44 +0530

    Add solution for Diamond Problem and Method Resolution Order

---
+++ diamond-problem-and-method-resolution-order/solution.py
@@ -0,0 +1,30 @@
+"""
+Problem: Diamond Problem and Method Resolution Order
+URL: https://neetcode.io/problems/python-diamond-problem-and-method-resolution-order/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+class A:
+    def print_method(self) -> None:
+        print("A")
+
+class B(A):
+    def print_method(self) -> None:
+        print("B")
+
+class C(A):
+    def print_method(self) -> None:
+        print("C")
+
+# class D(C, B): 
+#     pass
+
+
+# Do not change the code below
+d = D()
+d.print_method()
+class D(B,C):
+    pass
+print(D.__mro__)
