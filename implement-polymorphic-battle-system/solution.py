39d6edcd9b42cdee9215640c5b64bc44edd078a7
Shivansh Kumar <104713531+ShivanshKumar760@users.noreply.github.com>
2026-01-30 21:14:31 +0530

    Update solution for Implement Polymorphic Battle System

--- implement-polymorphic-battle-system/solution.py
+++ implement-polymorphic-battle-system/solution.py
@@ -9,7 +9,7 @@ Solution by NeetCode GitHub Pusher
         return self.power+10
 class Mage(Hero):
     def __init__(self,name,power):
-        super.__init__(self,name,power)
+        super.__init__(name,power)
         self.health-=20
     def attack(self):
         return self.power+20
