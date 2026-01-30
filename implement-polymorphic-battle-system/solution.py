f4f57e44df8d81563e604813099dddb913bacaeb
Shivansh Kumar <104713531+ShivanshKumar760@users.noreply.github.com>
2026-01-30 21:14:06 +0530

    Update solution for Implement Polymorphic Battle System

--- implement-polymorphic-battle-system/solution.py
+++ implement-polymorphic-battle-system/solution.py
@@ -9,11 +9,11 @@ Solution by NeetCode GitHub Pusher
         return self.power+10
 class Mage(Hero):
     def __init__(self,name,power):
-        super.__init__()
+        super.__init__(self,name,power)
         self.health-=20
     def attack(self):
         return self.power+20
 # TODO: Implement the battle function
 def show_attack(hero):
-    print(f"{hero.name} attack with {hero.attack} damage")
     def attack(self):
+class Warrior(Hero):
