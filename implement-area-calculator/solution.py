27b790f5dc641d20aa6e85744deaaa6ae965c429
Shivansh Kumar <104713531+ShivanshKumar760@users.noreply.github.com>
2026-01-31 16:15:21 +0530

    Add solution for Implement Area Calculator

---
+++ implement-area-calculator/solution.py
@@ -0,0 +1,26 @@
+"""
+Problem: Implement Area Calculator
+URL: https://neetcode.io/problems/python-implement-area-calculator/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+import math
+
+class AreaCalc:
+    # TODO: Implement calculate method
+    # pass
+
+
+
+# Don't modify the following code
+calc = AreaCalc()
+print(calc.calculate(5))    
+print(calc.calculate(4, 6))
+    def calculate(length,width=None):
+        if(width=None):
+            return round(math.pi*(l**2))
+        else:
+
+            return length*width
