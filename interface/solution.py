c08d7bb8d36ad736071846b86183c48a3fc74108
Shivansh Kumar <104713531+ShivanshKumar760@users.noreply.github.com>
2026-01-31 19:29:16 +0530

    Add solution for Interface

---
+++ interface/solution.py
@@ -0,0 +1,37 @@
+"""
+Problem: Interface
+URL: https://neetcode.io/problems/python-interface/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+from abc import ABC, abstractmethod
+
+# TODO: Create a Superhero interface
+
+
+class Superman(Superhero):
+    def fly(self) -> str:
+        return "Up, up and away!"
+
+    def use_power(self) -> str:
+        return "Using heat vision"
+
+class WonderWoman(Superhero):
+    def fly(self) -> str:
+        return "Soaring through the clouds!"
+
+    def use_power(self) -> str:
+        return "Using lasso of truth"
+
+
+
+# Don't modify the code below
+class SuperHero(ABC):
+    @abstractmethod
+    def fly(self)-str:
+        pass
+    def use_power(self)->str:
+        pass
+superman = Superman()
